da2ab350ac3f71820dc1e260ac54687ce2d27d5f
toxuh <zakharov.toxa@gmail.com>
2020-02-06 20:55:27 +0100

    Updates

---
+++ precache-manifest.eda67662138f15a120e78e0e68ba9112.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cdc061b62b07cd59be1663703f189c4",
+    "url": "/comp_man/index.html"
+  },
+  {
+    "revision": "f790576e6643c2d08a24",
+    "url": "/comp_man/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "4e6d60546cb99dc8595f",
+    "url": "/comp_man/static/css/main.3084b84f.chunk.css"
+  },
+  {
+    "revision": "f790576e6643c2d08a24",
+    "url": "/comp_man/static/js/2.f0b08c00.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/comp_man/static/js/2.f0b08c00.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4e6d60546cb99dc8595f",
+    "url": "/comp_man/static/js/main.b3e9105f.chunk.js"
+  },
+  {
+    "revision": "97dedd8c9537295595c8",
+    "url": "/comp_man/static/js/runtime-main.4d300ca9.js"
+  }
+]);
